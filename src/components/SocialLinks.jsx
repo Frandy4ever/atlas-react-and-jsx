4f841d666911d187e93d4c137281ae244ce03d49
Frandy Slueue <frandy4ever@gmail.com>
2025-01-08 11:30:08 -0600

    Add SocialLinks component

---
+++ src/components/SocialLinks.jsx
@@ -0,0 +1,20 @@
+export default function SocialLinks() {
+  return (
+    <>
+      <div className="social-links">
+        <a
+          href="https://github.com/Frandy4ever/atlas-react-and-jsx/tree/main/src"
+          target="_blank"
+        >
+          <img src="src/assets/linkedin.svg" alt="" />
+        </a>
+        <a
+          href="https://github.com/Frandy4ever/atlas-react-and-jsx/tree/main/src"
+          target="_blank"
+        >
+          <img src="src/assets/github.svg" alt="" />
+        </a>
+      </div>
+    </>
+  );
+}
